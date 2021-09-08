5bc57f3ca290502560a600a2143217011b9f5c58
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-09-08 16:38:32 +0300

    Wednesday work

---
+++ functions.py
@@ -0,0 +1,17 @@
+
+#Function to remove outliers using z-score
+def remove_outliers(data, col):
+    col_upper = data[col].mean() + 3*data[col].std()
+    col_lower = data[col].mean() - 3*data[col].std()
+    df = data[(data[col]< col_upper) & (data[col]> col_lower)]
+    return df 
+
+
+#Function to remove outliers using percentile 
+
+def percentile_based_outlier(data, col):
+    upper_limit = data[col].quantile(0.975)
+    lower_limit = data[col].quantile(0.025)
+    df_percentile = data[(data[col]< upper_limit) & (data[col]> lower_limit)]
+    return df_percentile 
+    
